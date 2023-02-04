6699c073448ca2a76dff62848988bb0e88aa9b06
Liusp <thodsaphonsp@gmail.com>
2023-02-04 17:22:33 +0700

    add logout

--- MvcClient/Controllers/HomeController.cs
+++ MvcClient/Controllers/HomeController.cs
@@ -35,5 +35,9 @@ namespace MvcClient.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult Logout()
+        {
+            return SignOut("Cookies", "oidc");
+        }
     }
 }
