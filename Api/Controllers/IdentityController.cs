d5b5bc86a604d77551b478cfcfb3081719af1c22
Liusp <thodsaphonsp@gmail.com>
2023-02-04 15:22:47 +0700

    add policy to authen

--- Api/Controllers/IdentityController.cs
+++ Api/Controllers/IdentityController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Api.Controllers
 {
     [Route("identity")]
-    [Authorize]
+    [Authorize("ApiScope")]
     public class IdentityController : ControllerBase
     {
         [HttpGet]
