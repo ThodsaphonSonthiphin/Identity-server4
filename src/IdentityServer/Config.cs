dfe6cc916e6c85194fa52bad725250d2007170ce
Liusp <thodsaphonsp@gmail.com>
2023-02-04 13:27:35 +0700

    config client

--- src/IdentityServer/Config.cs
+++ src/IdentityServer/Config.cs
@@ -21,8 +21,30 @@ namespace IdentityServer
                 new ApiScope("api1", "My API")
             };
 
-        public static IEnumerable<Client> Clients =>
-            new Client[] 
-            { };
+        public static IEnumerable<Client> Clients
+        {
+            get
+            {
+                return new List<Client>
+                {
+                    new Client
+                    {
+                        ClientId = "client",
+
+                        // no interactive user, use the clientid/secret for authentication
+                        AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                        // secret for authentication
+                        ClientSecrets =
+                        {
+                            new Secret("secret".Sha256())
+                        },
+
+                        // scopes that client has access to
+                        AllowedScopes = { "api1" }
+                    }
+                };
+            }
+        }
     }
 }
